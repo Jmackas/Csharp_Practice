46731264a15ce21e225f7d642b9ad2c43706bdbb
Jmackas <jamesrossmackay@gmail.com>
2018-10-18 18:56:58 +0800

    Spam Email Lab

---
+++ James M Lab 1/Spam Email Check/Spam Scanner/Spam Scanner/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spam_Scanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Counter variable declaration
+            double counter = 0;
+
+            // 30 Common spam words and phrases to an array (source - https://emailmarketing.comm100.com/email-marketing-ebook/spam-words.aspx)
+            string[] spamTerms = { "Money", "No questions asked", "4U", "Winner", "Click below", "Compare rates", "Free website", "Free offer", "Free investment", "Do it today", "No selling", "Save $", "Opt in", "Stop snoring", "No age restrictions", "Pure profit", "We honor all", "You have been selected", "We hate spam", "Your income", "Winning", "They keep your money — no refund!", "Unlimited", "Viagra and other drugs", "Urgent", "No medical exams", "Multi level marketing", "Outstanding values", "Strong buy", "Stuff on sale" };
+
+            // Make all terms lower case in the array
+            spamTerms = spamTerms.Select(s => s.ToLowerInvariant()).ToArray();
+
+            // Take user input
+            Console.WriteLine("Please enter an e-mail message");
+            var userSpam = Console.ReadLine();
+            var userSpamLower = userSpam.ToLower();
+
+            // Cycling the message through the various spam terms
+            foreach (string i in spamTerms)
+            {
+                // Determining if the string is contained within the e-mail
+                if (userSpamLower.Contains(i))
+                {
+                    counter++;
+                }
+            }
+
+            // Spam score algorithm
+            double spamScore = (counter / 30) * 100;
+            
+            // Spam score output
+            Console.WriteLine("The email is currently " + spamScore + "% spam");
+
+            // Determining if the message is spam
+            if (spamScore > 10)
+            {
+                Console.WriteLine("This message is most likely spam.");
+            }
+
+            else if (spamScore < 10) 
+            {
+                Console.WriteLine("This is most likely not spam.");
+            }
+
+        }
+    }
+}
---
+++ James M Lab 1/Spam Email Check/Spam Scanner/Spam Scanner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Spam Scanner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Spam Scanner")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dbf3c46a-c8df-43fa-aaf2-bfacd624e6cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
