f71cefd159a263affd300e19abdae11a72b09273
James <jamesrossmackay@gmail.com>
2018-10-25 07:08:47 +0800

    Lab test files added

---
+++ Lab 2 Test Practice/_Main/Lab Test Files/LabTest2/LabTest2/CashPurchase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTest2
+{
+   
+    
+        class CashPurchase : Purchase
+        {
+
+            private double discount { get; set; } // discount for cash purchase
+            // constructor initializes data members
+            public CashPurchase(string scode, string icode, double q, double cp, double dis)
+                : base(scode, icode, q, cp)
+            {
+                discount = dis;
+            }
+
+            public CashPurchase()
+                : base()
+            { }
+            //Discount property
+            public double Discount
+            {
+                get
+                {
+                    return discount;
+                } // end get
+                set
+                {
+                    if (value >= 0.0)
+                        discount = value;
+                    else
+                        throw new ArgumentOutOfRangeException("Discount", value, "Discount must be >= 0");
+
+                } // end set
+            } // end property
+
+            public override double calculateCost()
+            {
+                double cost = base.calculateCost();
+                double netCost = cost - cost * Discount;
+                return netCost;
+            } // end function calculateCost
+
+            public override string ToString()
+            {
+                return string.Format("Purchase {0} {1} \n{2:C}", base.ToString(), "Discount", Discount.ToString(), "Cost", calculateCost().ToString());
+            }
+
+
+        }
+    }
+
