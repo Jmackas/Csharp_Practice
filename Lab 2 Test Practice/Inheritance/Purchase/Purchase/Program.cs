f86751383c4361ecf374668864ec5034bc6f4617
Jmackas <jamesrossmackay@gmail.com>
2018-10-28 11:20:47 +0800

    Update

--- Lab 2 Test Practice/Inheritance/Purchase/Purchase/Program.cs
+++ Lab 2 Test Practice/Inheritance/Purchase/Purchase/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Purchase
+namespace BentleyLabTest2Alt
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Purchase purchase = new Purchase("test", "esketit", 1.0, 8.9);
+
+            Console.WriteLine(purchase.Quantity);
+            Console.WriteLine(purchase.Quantity);
+            Console.WriteLine(purchase.Quantity);
+            Console.WriteLine(purchase.Quantity);
 
         }
     }
