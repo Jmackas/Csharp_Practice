54030dc6f0d60896ca7753a7bea0c281e1988c5a
Jmackas <jamesrossmackay@gmail.com>
2018-10-28 17:08:57 +0800

    Update

--- Lab 2 Test Practice/_Main/Potential Lab Setups/Guess 1/Files Attached/Program.cs
+++ Lab 2 Test Practice/_Main/Potential Lab Setups/Guess 1/Files Attached/Program.cs
@@ -10,7 +10,19 @@ namespace LabTest2
     {
         static void Main(string[] args)
         {
-            
+            string line;
+            List Purchase = new List();
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"file.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] words = line.Split(',');
+                listOfPersons.Add(new Purchase(words[0], words[1], words[2], words[3]));
+            }
+
+
         }
     }
 }
